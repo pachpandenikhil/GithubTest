ac8b1c0d8f139c6434e25538c99babbd1c045515
pachpandenikhil <pachpand@usc.edu>
2016-12-23 20:36:59 -0800

    main updated
    main updated

--- src/com/github/test/Main.java
+++ src/com/github/test/Main.java
@@ -3,7 +3,7 @@ package com.github.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Initial Test");
+		System.out.println("Initial Test Updated");
 	}
 
 }
